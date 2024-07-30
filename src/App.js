c7d53442a56ac517ce710298738a66eac712ed9d
for_lack_of_a_better_name <vivekkr174@gmail.com>
2024-07-30 17:30:28 +0530

    signIn completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
     <div>
-      <SignUp />
+
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<SignIn/>} />
+      <Route path='/signup' element={<SignUp/>} />
+      {/* <Route path='/create' element={<Createpost/>} />
+      <Route path='/view' element={<Viewall/>} />
+      <Route path='/viewmy' element={<Viewmy/>} /> */}
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
